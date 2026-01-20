25b243cab12dad43212934a2cd063b44fd5b8b9e
Knarta <charlene.martin@holbertonstudents.com>
2026-01-20 15:23:11 +0100

    chore: update webpack configuration to define entry point as an object and disable performance hints

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
+  mode: 'production',
+  entry: {
+    main: './js/dashboard_main.js',
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
-  mode: 'production',
+  performance: {
+    hints: false,
+  },
   module: {
     rules: [
       {
