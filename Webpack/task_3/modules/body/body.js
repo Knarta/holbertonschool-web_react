1adf91a53c34dffe255e0b195444851b2a2d15d4
Knarta <charlene.martin@holbertonstudents.com>
2026-01-20 16:31:36 +0100

    style: format body.js for improved readability and consistency

--- Webpack/task_3/modules/body/body.js
+++ Webpack/task_3/modules/body/body.js
@@ -3,14 +3,15 @@ import './body.css';
 import _ from 'lodash';
 
 let count = 0;
+
 function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
+  count++;
+  $('#count').text(`${count} clicks on the button`);
 }
 
-    $(function() {
-        $('body').append('<p>Dashboard data for the students</p>');
-        $('body').append(`<button>Click here to get started</button>`);
-        $('body').append(`<p id="count"></p>`);
-        $('button').on('click', _.debounce(updateCounter, 500));
+$(function () {
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('button').on('click', _.debounce(updateCounter, 500));
 });
