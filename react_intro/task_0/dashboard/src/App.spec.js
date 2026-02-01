12b18e5e2375111096f13377a016bdbfc1a9fb89
Knarta <charlene.martin@holbertonstudents.com>
2026-02-01 11:42:01 +0100

    test: update App component unit tests to check specific text content in body and footer paragraphs

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -9,9 +9,9 @@ test('renders School Dashboard h1', () => {
 
 test('renders correct text content p elements app-body and app-footer', () => {
     render(<App />);
-    const Body_p = screen.getByText(/./, { selector: 'p' });
+    const Body_p = screen.getByText('Login to access the full dashboard');
     expect(Body_p).toBeInTheDocument();
-    const Footer_p = screen.getByText(/./, { selector: 'p' });
+    const Footer_p = screen.getByText('Copyright 2026 - Holberton School');
     expect(Footer_p).toBeInTheDocument();
 });
 
