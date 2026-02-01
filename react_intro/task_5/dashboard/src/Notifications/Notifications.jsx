bf3743b03c0130b1bde6cab6137405589840fba4
Knarta <charlene.martin@holbertonstudents.com>
2026-02-01 18:12:39 +0100

    refactor: simplify Notifications component and update styles for better visual hierarchy

--- react_intro/task_5/dashboard/src/Notifications/Notifications.jsx
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.jsx
@@ -1,22 +1,36 @@
-import closeIcon from './assets/close-button.png';
-import { getLatestNotification } from '../utils/utils.js';
-
-
+import closeButton from "../assets/close-button.png";
+import "./Notifications.css";
+import { getLatestNotification } from "../utils/utils.js";
 
 function Notifications() {
-    return (
-        <div className="notification-items">
-            <p>Here is the list of notifications</p>
-                <ul>
-                    <li data-priority="default">New course available</li>
-                    <li data-priority="urgent">New resume available</li>
-                    <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
-                </ul>
-                <button aria-label="Close" onClick={() => console.log('Close button has been clicked')}>
-                    <img src={closeIcon} alt="close button" />
-                </button>
-        </div>
-     );
+  const markup = { __html: getLatestNotification() };
+  return (
+    <>
+      <div className="notification-items">
+        <p>Here is the list of notifications</p>
+        <ul>
+          <li data-priority="default">New course available</li>
+          <li data-priority="urgent">New resume available</li>
+          <li data-priority="urgent" dangerouslySetInnerHTML={markup}></li>
+        </ul>
+      </div>
+      <button
+        style={{
+          width: "8px",
+          height: "8px",
+          border: "0px",
+          background: "none",
+          position: "absolute",
+          top: "10px",
+          right: "20px",
+        }}
+        aria-label="Close"
+        onClick={() => console.log("Close button has been clicked")}
+      >
+        <img src={closeButton} alt="close-button" />
+      </button>
+    </>
+  );
 }
 
 export default Notifications;
