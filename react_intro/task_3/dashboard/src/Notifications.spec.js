d4310d85d82ee58aaa7610d5ae89e11679877233
Knarta <charlene.martin@holbertonstudents.com>
2026-02-01 15:18:20 +0100

    test: add unit tests for Notifications component to verify rendering and functionality

---
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import Notifications from './Notifications';
+
+
+test("Notifications exists", () => {
+    render(<Notifications />);
+    const notifications = screen.getByText(/Here is the list of notifications/i);
+    expect(notifications).toBeInTheDocument();
+});
+
+test("Button element exists", () => {
+    render(<Notifications />);
+    const button = screen.getByRole('button', { name: /Close/i });
+    expect(button).toBeInTheDocument();
+});
+
+test("renders 3 li elements as notifications", () => {
+    render(<Notifications />);
+    const notifications = screen.getAllByRole('listitem');
+    expect(notifications).toHaveLength(3);
+});
+
+test('clicking close button logs "Close button has been clicked"', () => {
+    const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+    render(<Notifications />);
+    const button = screen.getByRole('button', { name: /Close/i });
+    button.click();
+    expect(console.log).toHaveBeenCalledWith('Close button has been clicked');
+    consoleSpy.mockRestore();
+});
