4e377f0bc6e76f5e35c4e1e7128ff8f4262ccbad
Knarta <charlene.martin@holbertonstudents.com>
2026-02-03 12:28:27 +0100

    feat: integrate notifications list into App component to display multiple notifications in the React dashboard

--- react_props/task_1/dashboard/src/App/App.jsx
+++ react_props/task_1/dashboard/src/App/App.jsx
@@ -3,11 +3,21 @@ import Notifications from "../Notifications/Notifications.jsx";
 import Header from "../Header/Header.jsx";
 import Login from "../Login/Login.jsx";
 import Footer from "../Footer/Footer.jsx";
+import { getLatestNotification } from "../utils/utils.js";
 
 function App() {
+  const notificationsList = [
+    {
+      id: 1, title: "New course available", type: "default", value: "New course available"},
+    {
+      id: 2, title: "New resume available", type: "urgent", value: "New resume available"},
+    {
+      id: 3, title: "New course available", type: "urgent", html: getLatestNotification()
+    },
+  ];
   return (
     <>
-      <Notifications />
+      <Notifications notification={notificationsList} />
       <Header />
       <Login />
       <Footer />
