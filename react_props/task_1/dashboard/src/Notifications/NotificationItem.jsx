a6f6531ef910efbce2f51a043803e0891cab7cc9
Knarta <charlene.martin@holbertonstudents.com>
2026-02-03 13:08:52 +0100

    refactor: clean up NotificationItem component for improved readability and consistency in JSX formatting

--- react_props/task_1/dashboard/src/Notifications/NotificationItem.jsx
+++ react_props/task_1/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,20 +1,24 @@
-function NotificationItem ({ type , html , value }) {
+function NotificationItem({ type, html, value }) {
     const style = {
-        color: type === "urgent" ? "red" : "blue"
-    }
-
-    const text = type === "urgent" ? "New resume available" : "New course available";
-
+      color: type === "urgent" ? "red" : "blue",
+    };
+  
     if (html) {
-        return (
-            <li data-notification-type={type} style={style} dangerouslySetInnerHTML={typeof html === 'object' ? html : { __html: html }}></li>
-            );
-        }
-    else {
-        return (
-            <li data-notification-type={type} style={style}>{text}</li>
-            );
+      return (
+        <li
+          data-notification-type={type}
+          style={style}
+          dangerouslySetInnerHTML={typeof html === "object" ? html : { __html: html }}
+        />
+      );
+    } else {
+      return (
+        <li data-notification-type={type} style={style}>
+          {value}
+        </li>
+      );
     }
-}
-
-export default NotificationItem;
+  }
+  
+  export default NotificationItem;
+  
