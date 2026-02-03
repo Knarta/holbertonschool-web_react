2dd5dacb1534e3022e6144fdccd895a4b3adc6ff
Knarta <charlene.martin@holbertonstudents.com>
2026-02-03 12:28:31 +0100

    feat: add NotificationItem component to render individual notifications with dynamic styling in the React dashboard

---
+++ react_props/task_1/dashboard/src/Notifications/NotificationItem.jsx
@@ -0,0 +1,18 @@
+function NotificationItem ({ type , html , value }) {
+    const style = {
+        color: type === "urgent" ? "blue" : "red"
+    }
+
+    if (html) {
+        return (
+            <li data-notification-type={type} style={style} dangerouslySetInnerHTML={{ __html: html }}></li>
+            );
+        }
+    else {
+        return (
+            <li data-notification-type={type} style={style}>{value}</li>
+            );
+    }
+}
+
+export default NotificationItem;
