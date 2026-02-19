ff1e75b060d916aa53afc238e870f0a21e204fa7
Knarta <charlene.martin@holbertonstudents.com>
2026-02-19 14:15:45 +0100

    refactor: enhance CourseList component to conditionally render course availability and integrate WithLogging HOC

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -1,50 +1,45 @@
-import CourseListRow from './CourseListRow';
+import CourseListRow from "./CourseListRow";
+import WithLogging from "../HOC/WithLogging.jsx";
 
 function CourseList({ courses = [] }) {
-  if (courses.length === 0) {
-    return (
-      <div className="courses-container w-4/5 mx-auto p-8">
-        <table
-          id="noCourse"
-          className="w-full min-w-full border border-black border-collapse text-center font-bold [&_th]:font-bold [&_th]:text-center [&_tr]:font-bold [&_th:first-child]:w-[60%] [&_td:first-child]:w-[60%]"
-        >
-          <tbody>
+  return (
+    <div className="courses-container w-4/5 mx-auto">
+      <table id="CourseList" className="w-full border-collapse">
+        {courses.length === 0 ? (
+          <thead>
             <CourseListRow
               textFirstCell="No course available yet"
-              isHeader={false}
+              isHeader={true}
             />
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-  return (
-    <div className="courses-container w-4/5 mx-auto p-8">
-      <table
-        id="coursesTable"
-        className="w-full min-w-full border border-black border-collapse [&_th]:font-bold [&_th]:text-center [&_tr]:font-bold [&_th:first-child]:w-[60%] [&_td:first-child]:w-[60%]"
-      >
-        <thead>
-          <CourseListRow textFirstCell="Available courses" isHeader={true} />
-          <CourseListRow
-            textFirstCell="Course name"
-            textSecondCell="Credit"
-            isHeader={true}
-          />
-        </thead>
-        <tbody>
-          {courses.map((course) => (
-            <CourseListRow
-              key={course.id}
-              textFirstCell={course.name}
-              textSecondCell={course.credit}
-              isHeader={false}
-            />
-          ))}
-        </tbody>
+          </thead>
+        ) : (
+          <>
+            <thead>
+              <CourseListRow
+                textFirstCell="Available courses"
+                isHeader={true}
+              />
+              <CourseListRow
+                textFirstCell="Course name"
+                textSecondCell="Credit"
+                isHeader={true}
+              />
+            </thead>
+            <tbody>
+              {courses.map((course) => (
+                <CourseListRow
+                  key={course.id}
+                  textFirstCell={course.name}
+                  textSecondCell={course.credit}
+                  isHeader={false}
+                />
+              ))}
+            </tbody>
+          </>
+        )}
       </table>
     </div>
   );
 }
 
-export default CourseList;
+export default WithLogging(CourseList);
