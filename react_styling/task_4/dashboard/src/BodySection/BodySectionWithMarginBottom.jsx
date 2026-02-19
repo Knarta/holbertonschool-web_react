0141f9e6e41852a740492f98fa80be62e9c782aa
Knarta <charlene.martin@holbertonstudents.com>
2026-02-19 16:39:00 +0100

    feat: implement core dashboard components including App, Header, Footer, Login, CourseList, Notifications, and styling with Tailwind CSS for enhanced user experience

---
+++ react_styling/task_4/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BodySection from './BodySection';
+
+const BodySectionWithMargin = ({ title, children }) => (
+  <div className="bodySectionWithMargin mb-10">
+    <BodySection title={title}>{children}</BodySection>
+  </div>
+);
+
+export default BodySectionWithMargin;
