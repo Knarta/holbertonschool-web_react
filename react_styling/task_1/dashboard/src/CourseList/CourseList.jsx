564de55a1e490c0a8e9ff2787e424e55568e782a
Knarta <charlene.martin@holbertonstudents.com>
2026-02-19 14:31:48 +0100

    refactor: improve CourseList component to handle empty course scenarios and enhance table structure

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -2,41 +2,47 @@ import CourseListRow from "./CourseListRow";
 import WithLogging from "../HOC/WithLogging.jsx";
 
 function CourseList({ courses = [] }) {
-  return (
-    <div className="courses-container w-4/5 mx-auto">
-      <table id="CourseList" className="w-full border-collapse">
-        {courses.length === 0 ? (
-          <thead>
+  if (courses.length === 0) {
+    return (
+      <div className="courses-container w-4/5 mx-auto p-8">
+        <table
+          id="noCourse"
+          className="w-full min-w-full border border-black border-collapse text-center font-bold [&_th]:font-bold [&_th]:text-center [&_tr]:font-bold [&_th:first-child]:w-[60%] [&_td:first-child]:w-[60%]"
+        >
+          <tbody>
             <CourseListRow
               textFirstCell="No course available yet"
-              isHeader={true}
+              isHeader={false}
             />
-          </thead>
-        ) : (
-          <>
-            <thead>
-              <CourseListRow
-                textFirstCell="Available courses"
-                isHeader={true}
-              />
-              <CourseListRow
-                textFirstCell="Course name"
-                textSecondCell="Credit"
-                isHeader={true}
-              />
-            </thead>
-            <tbody>
-              {courses.map((course) => (
-                <CourseListRow
-                  key={course.id}
-                  textFirstCell={course.name}
-                  textSecondCell={course.credit}
-                  isHeader={false}
-                />
-              ))}
-            </tbody>
-          </>
-        )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  return (
+    <div className="courses-container w-4/5 mx-auto p-8">
+      <table
+        id="coursesTable"
+        className="w-full min-w-full border border-black border-collapse [&_th]:font-bold [&_th]:text-center [&_tr]:font-bold [&_th:first-child]:w-[60%] [&_td:first-child]:w-[60%]"
+      >
+        <thead>
+          <CourseListRow textFirstCell="Available courses" isHeader={true} />
+          <CourseListRow
+            textFirstCell="Course name"
+            textSecondCell="Credit"
+            isHeader={true}
+          />
+        </thead>
+        <tbody>
+          {courses.map((course) => (
+            <CourseListRow
+              key={course.id}
+              textFirstCell={course.name}
+              textSecondCell={course.credit}
+              isHeader={false}
+            />
+          ))}
+        </tbody>
       </table>
     </div>
   );
