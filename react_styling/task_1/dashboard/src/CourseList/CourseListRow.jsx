657afb9d47706e61a1ec793ffb52a4f7a027720f
Knarta <charlene.martin@holbertonstudents.com>
2026-02-19 14:15:50 +0100

    test: add unit tests for CourseListRow component to verify header and cell rendering

--- react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,29 +1,61 @@
-function CourseListRow({
-    isHeader = false,
-    textFirstCell = '',
-    textSecondCell = null,
-  }) {
-    if (isHeader) {
-      if (textSecondCell === null) {
-        return (
-          <tr className="bg-[var(--color-table-header)/66%]">
-            <th colSpan={2} className="border border-gray-400 px-2">{textFirstCell}</th>
-          </tr>
-        );
-      }
-      return (
-        <tr className="bg-[var(--color-table-header)/66%]">
-          <th className="border border-gray-400 px-2">{textFirstCell}</th>
-          <th className="border border-gray-400 px-2">{textSecondCell}</th>
-        </tr>
-      );
-    }
-    return (
-      <tr className="bg-[var(--color-table-rows)/45%]">
-        <td className="border border-gray-400 px-2">{textFirstCell}</td>
-        <td className="border border-gray-400 px-2">{textSecondCell}</td>
-      </tr>
-    );
-  }
-  
-  export default CourseListRow;
+import { render, screen } from '@testing-library/react';
+import CourseListRow from './CourseListRow.jsx';
+
+test('isHeader true and textSecondCell null', () => {
+  render(
+    <>
+      <table>
+        <thead>
+          <CourseListRow
+            isHeader={true}
+            textFirstCell="Testing"
+            textSecondCell={null}
+          />
+        </thead>
+      </table>
+    </>,
+  );
+
+  const tableHeader = screen.getByText('Testing');
+
+  expect(screen.getAllByRole('columnheader').length).toBe(1);
+  // expect(tableHeader.colSpan).toBe(2)
+  expect(tableHeader).toHaveAttribute('colspan', '2');
+});
+
+test('isHeader true and textSecondCell is not null', () => {
+  render(
+    <>
+      <table>
+        <thead>
+          <CourseListRow
+            isHeader={true}
+            textFirstCell="Testing"
+            textSecondCell="Testing 2"
+          />
+        </thead>
+      </table>
+    </>,
+  );
+
+  expect(screen.getAllByRole('columnheader').length).toBe(2);
+});
+
+test('should render two td if isHeader false', () => {
+  render(
+    <>
+      <table>
+        <tbody>
+          <CourseListRow
+            isHeader={false}
+            textFirstCell="Testing"
+            textSecondCell="Testing2"
+          />
+        </tbody>
+      </table>
+    </>,
+  );
+
+  expect(screen.getAllByRole('row').length).toBe(1);
+  expect(screen.getAllByRole('cell').length).toBe(2);
+});
