30c3a707e57ea5982900fb17dc59f672631afd89
Knarta <charlene.martin@holbertonstudents.com>
2026-02-19 14:54:13 +0100

    refactor: optimize CourseList component to conditionally render course availability and improve table structure

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -2,49 +2,34 @@ import CourseListRow from "./CourseListRow";
 import WithLogging from "../HOC/WithLogging.jsx";
 
 function CourseList({ courses = [] }) {
-  if (courses.length === 0) {
-    return (
-      <div className="courses-container mx-auto p-8" style={{ width: '80%' }}>
-        <table
-          id="noCourse"
-          className="border border-black border-collapse text-center font-bold [&_th]:font-bold [&_th]:text-center [&_tr]:font-bold [&_th:first-child]:w-[60%] [&_td:first-child]:w-[60%]"
-          style={{ width: '100%' }}
-        >
-          <tbody>
-            <CourseListRow
-              textFirstCell="No course available yet"
-              isHeader={false}
-            />
-          </tbody>
-        </table>
-      </div>
-    );
-  }
   return (
-    <div className="courses-container mx-auto p-8" style={{ width: '80%' }}>
+    <div className="courses-container w-[80%] mx-auto p-8"> 
       <table
-        id="coursesTable"
-        className="border border-black border-collapse [&_th]:font-bold [&_th]:text-center [&_tr]:font-bold [&_th:first-child]:w-[60%] [&_td:first-child]:w-[60%]"
-        style={{ width: '100%' }}
+        id={courses.length === 0 ? "noCourse" : "coursesTable"}
+        className="w-full border border-gray-400 border-collapse text-center font-bold [&_th]:font-bold [&_th]:text-center [&_tr]:font-bold [&_th:first-child]:w-[60%] [&_td:first-child]:w-[60%]"
       >
-        <thead>
-          <CourseListRow textFirstCell="Available courses" isHeader={true} />
-          <CourseListRow
-            textFirstCell="Course name"
-            textSecondCell="Credit"
-            isHeader={true}
-          />
-        </thead>
-        <tbody>
-          {courses.map((course) => (
-            <CourseListRow
-              key={course.id}
-              textFirstCell={course.name}
-              textSecondCell={course.credit}
-              isHeader={false}
-            />
-          ))}
-        </tbody>
+        {courses.length === 0 ? (
+          <tbody>
+            <CourseListRow textFirstCell="No course available yet" isHeader={false} />
+          </tbody>
+        ) : (
+          <>
+            <thead>
+              <CourseListRow textFirstCell="Available courses" isHeader={true} />
+              <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+            </thead>
+            <tbody>
+              {courses.map((course) => (
+                <CourseListRow
+                  key={course.id}
+                  textFirstCell={course.name}
+                  textSecondCell={course.credit}
+                  isHeader={false}
+                />
+              ))}
+            </tbody>
+          </>
+        )}
       </table>
     </div>
   );
