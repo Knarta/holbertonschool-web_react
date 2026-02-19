2cc3d87541b2ed2ca76c625278a21ed8a5695423
Knarta <charlene.martin@holbertonstudents.com>
2026-02-19 16:03:09 +0100

    style: remove unnecessary margin-top from Footer component for cleaner layout

--- react_styling/task_3/dashboard/src/Footer/Footer.jsx
+++ react_styling/task_3/dashboard/src/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { getCurrentYear, getFooterCopy } from "../utils/utils.js";
 
 function Footer() {
   return (
-    <footer className="App-footer flex justify-center border-t-2 border-[var(--main-color)] mt-auto py-4 text-center">
+    <footer className="App-footer flex justify-center border-t-2 border-[var(--main-color)] py-4 text-center">
       <p className="text-sm italic">
         Copyright {getCurrentYear()} - {getFooterCopy(true)}
       </p>
