4972110a26601efa2b5c218e4f55bab1c76a96e7
Knarta <charlene.martin@holbertonstudents.com>
2026-03-09 12:06:10 +0100

    feat: integrate user authentication context in App component, manage user state with logIn and logOut methods, and update rendering logic based on authentication status

--- react_state/task_3/dashboard/src/App/App.jsx
+++ react_state/task_3/dashboard/src/App/App.jsx
@@ -7,11 +7,19 @@ import Footer from '../Footer/Footer.jsx';
 import CourseList from '../CourseList/CourseList.jsx';
 import BodySectionWithMargin from '../BodySection/BodySectionWithMarginBottom.jsx';
 import BodySection from '../BodySection/BodySection.jsx';
+import AppContext from '../Context/context.js';
 
 class App extends React.Component {
-  static defaultProps = {
-    logOut: () => {},
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      },
+    };
+  }
 
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyDown);
@@ -24,16 +32,37 @@ class App extends React.Component {
   handleKeyDown = (event) => {
     if (event.ctrlKey && event.key === 'h') {
       alert('Logging you out');
-      this.logout();
+      this.logOut();
     }
   };
 
-  logout() {
-    this.props.logOut();
-  }
+  logOut = () => {
+    this.setState({
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      },
+    });
+  };
+
+  logIn = (email, password) => {
+    this.setState({
+      user: {
+        email,
+        password,
+        isLoggedIn: true,
+      },
+    });
+  };
 
   render() {
-    const { isLoggedIn = false } = this.props;
+    const { user } = this.state;
+    const contextValue = {
+      user,
+      logOut: this.logOut,
+    };
+
     const notificationsList = [
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New resume available' },
@@ -47,29 +76,35 @@ class App extends React.Component {
     ];
 
     return (
-      <div className="min-h-screen flex flex-col w-full px-4 sm:px-6 md:px-8">
-        <Notifications notifications={notificationsList} />
-        <Header />
-        <main className="flex-1 flex flex-col">
-          {isLoggedIn ? (
-            <BodySectionWithMargin title="Course list">
-              <CourseList courses={coursesList} />
-            </BodySectionWithMargin>
-          ) : (
-            <BodySectionWithMargin title="Log in to continue">
-              <Login />
-            </BodySectionWithMargin>
-          )}
-          <Footer />
-          <BodySectionWithMargin />
-          <BodySection title="News from the School">
-            <>
-            <p>ipsum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, asperiores architecto blanditiis fuga doloribus sit illum aliquid ea distinctio minus accusantium, impedit quo voluptatibus ut magni dicta. Recusandae, quia dicta?</p>
-            <p>Holberton School News goes here</p>
-          </>
-          </BodySection>
-        </main>
-      </div>
+      <AppContext.Provider value={contextValue}>
+        <div className="min-h-screen flex flex-col w-full px-4 sm:px-6 md:px-8">
+          <Notifications notifications={notificationsList} />
+          <Header />
+          <main className="flex-1 flex flex-col">
+            {user.isLoggedIn ? (
+              <BodySectionWithMargin title="Course list">
+                <CourseList courses={coursesList} />
+              </BodySectionWithMargin>
+            ) : (
+              <BodySectionWithMargin title="Log in to continue">
+                <Login
+                  logIn={this.logIn}
+                  email={user.email}
+                  password={user.password}
+                />
+              </BodySectionWithMargin>
+            )}
+            <Footer />
+            <BodySectionWithMargin />
+            <BodySection title="News from the School">
+              <>
+                <p>ipsum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, asperiores architecto blanditiis fuga doloribus sit illum aliquid ea distinctio minus accusantium, impedit quo voluptatibus ut magni dicta. Recusandae, quia dicta?</p>
+                <p>Holberton School News goes here</p>
+              </>
+            </BodySection>
+          </main>
+        </div>
+      </AppContext.Provider>
     );
   }
 }
