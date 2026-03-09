7bd8ae1a027e4bd30c2461d9b528745705ff44a9
Knarta <charlene.martin@holbertonstudents.com>
2026-03-09 11:51:59 +0100

    feat: refactor Login component to manage state for email and password, implement form validation, and enable conditional submit button activation

--- react_state/task_2/dashboard/src/Login/Login.jsx
+++ react_state/task_2/dashboard/src/Login/Login.jsx
@@ -1,18 +1,74 @@
 import './Login.css';
+import React from 'react';
 
-function Login() {
-  return (
-    <div className="App-body">
-      <p>Login to access the full dashboard</p>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" autoComplete="email" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" autoComplete="current-password" />
-      </form>
-      <button>OK</button>
-    </div>
-  );
+class Login extends React.Component {
+  static defaultProps = {
+    email: '',
+    password: '',
+    logIn: () => {},
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: this.props.email,
+      password: this.props.password,
+      enableSubmit: false,
+    };
+  }
+
+  handleLoginSubmit = (e) => {
+    e.preventDefault();
+    this.props.logIn(this.state.email, this.state.password);
+  };
+
+  handleChangeEmail = (e) => {
+    const newEmail = e.target.value;
+    this.setState({ email: newEmail }, this.handleSubmitEnable);
+  };
+
+  handleChangePassword = (e) => {
+    const newPassword = e.target.value;
+    this.setState({ password: newPassword }, this.handleSubmitEnable);
+  };
+
+  handleSubmitEnable = () => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailValid =
+      this.state.email.length > 0 && regex.test(this.state.email);
+    const passwordValid = this.state.password.length >= 8;
+    if (emailValid && passwordValid) this.setState({ enableSubmit: true });
+    else this.setState({ enableSubmit: false });
+  };
+
+  render() {
+    return (
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form onSubmit={this.handleLoginSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            autoComplete="email"
+            value={this.state.email}
+            onChange={this.handleChangeEmail}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={this.state.password}
+            onChange={this.handleChangePassword}
+          />
+          <button type="submit" disabled={!this.state.enableSubmit}>
+            OK
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
